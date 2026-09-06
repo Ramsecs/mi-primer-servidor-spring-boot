c4db93fc992683051e87a348aee7feb533df0dcf
Ramsecs <ramsesn1506@gmail.com>
2026-09-05 22:51:26 -0700

    Feat: nueva version del proyecto
    Estilo css fue aplicado sobre el index y le dio un mejor estilo

---
+++ MiPrimerServidorDeAplicacion/README.md
@@ -0,0 +1,12 @@
+## Jerarquia de estilos
+
+### De menor a mayor prioridad
+
+1. Estilos del navegador 
+2. Estilos del head
+3. Estilos de etiqueta
+4. Clases 
+5. Hojas de estilo externas
+6. Estilos en linea (style en la etiqueta)
+7. !important
+8. Media queries
---
+++ MiPrimerServidorDeAplicacion/src/test/java/com/example/MiPrimerServidorDeAplicacion/MiPrimerServidorDeAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiPrimerServidorDeAplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerServidorDeAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
